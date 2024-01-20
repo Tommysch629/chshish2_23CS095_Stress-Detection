886bba20c7cc9d99bdeb361b010c1f64bdb15381
Tommy Shish <hinson2002.sch@gmail.com>
2024-01-20 20:50:51 +0800

    [Application] Stress Detection Application
    Real time detection using the model

---
+++ real_time_detection.py
@@ -0,0 +1,39 @@
+import os
+import cv2
+import tensorflow as tf
+from tensorflow import keras
+from keras.preprocessing import image
+from keras.preprocessing.image import load_img, img_to_array
+from keras.models import load_model
+import matplotlib.pyplot as plt
+import numpy as np
+
+model = load_model("best_val_acc_model.h5")
+cv2.ocl.setUseOpenCL(False)
+emotion_dict = {0: 'angry', 1: 'disgust', 2: 'fear', 3: 'happy', 4: 'neutral', 5: 'sad', 6: 'surprise'}
+cap = cv2.VideoCapture(0)
+facecasc = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break;
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = facecasc.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y - 50), (x + w, y + h + 10), (255, 0, 0), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+        prediction = model.predict(cropped_img)
+        maxindex = int(np.argmax(prediction))
+        print(list(prediction[0]))
+        cv2.putText(frame, emotion_dict[maxindex], (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2,
+                    cv2.LINE_AA)
+    cv2.imshow('Video', cv2.resize(frame, (640, 480), interpolation=cv2.INTER_CUBIC))
+    # press 'q' to quit webcam feed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
