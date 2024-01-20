891da8221123ec077dbe317f0c474b7e285cdd05
Tommy Shish <hinson2002.sch@gmail.com>
2024-01-20 20:38:20 +0800

    [Dataset] Update and new dataset
    split the fer2013 for testing and found new dataset(Asian) for futher testing

--- data_prepare.py
+++ data_prepare.py
@@ -4,13 +4,13 @@ from PIL import Image
 import os
 
 train_image_path = "data/train/"
+val_image_path = "data/val/"
 test_image_path = "data/test/"
 path = 'fer2013.csv'
 fer2013 = pd.read_csv(path)
 train_data = fer2013[fer2013['Usage'] == 'Training']
 public_test_data = fer2013[fer2013['Usage'] == 'PublicTest']
 private_test_data = fer2013[fer2013['Usage'] == 'PrivateTest']
-test_data = pd.concat([public_test_data, private_test_data], axis=0)
 emotion_type = {0: 'angry', 1: 'disgust', 2: 'fear', 3: 'happy', 4: 'sad', 5: 'surprise', 6: 'neutral'}
 
 for i, row in enumerate(train_data.index):
@@ -21,10 +21,18 @@ for i, row in enumerate(train_data.index):
     emotion_label = emotion_type[train_data.loc[row, 'emotion']]
     new_image.save(f"{train_image_path}{emotion_label}/{filename}")
 
-for i, row in enumerate(test_data.index):
-    image = np.fromstring(test_data.loc[row, 'pixels'], dtype=int, sep=' ')
+for i, row in enumerate(private_test_data.index):
+    image = np.fromstring(private_test_data.loc[row, 'pixels'], dtype=int, sep=' ')
     image = np.reshape(image, (48, 48))
     new_image = Image.fromarray(np.uint8(image),'L')
     filename = '{}.png'.format(i)
-    emotion_label = emotion_type[test_data.loc[row, 'emotion']]
+    emotion_label = emotion_type[private_test_data.loc[row, 'emotion']]
+    new_image.save(f"{val_image_path}{emotion_label}/{filename}")
+
+for i, row in enumerate(public_test_data.index):
+    image = np.fromstring(public_test_data.loc[row, 'pixels'], dtype=int, sep=' ')
+    image = np.reshape(image, (48, 48))
+    new_image = Image.fromarray(np.uint8(image),'L')
+    filename = '{}.png'.format(i)
+    emotion_label = emotion_type[public_test_data.loc[row, 'emotion']]
     new_image.save(f"{test_image_path}{emotion_label}/{filename}")
